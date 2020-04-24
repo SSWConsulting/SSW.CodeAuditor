295eb3415df6ac949ddf6a513a4bcb529dc2d657
anthony <nvhoanganh1909@gmail.com>
2020-04-25 01:42:06 +1000

    add sign up

--- ui/src/containers/Signup.svelte
+++ ui/src/containers/Signup.svelte
@@ -7,6 +7,25 @@
   import { navigateTo } from "svelte-router-spa";
 
   let loading;
+  const login = promise => {
+    serverError = "";
+    loading = true;
+    return promise
+      .catch(err => (serverError = err.message))
+      .finally(() => (loading = false));
+  };
+  const loginGmail = () =>
+    login(
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+    );
+
+  const loginFacebook = () =>
+    login(
+      firebase
+        .auth()
+        .signInWithRedirect(new firebase.auth.FacebookAuthProvider())
+    );
+
   const signup = () => {
     loading = true;
     serverError = "";
@@ -49,6 +68,31 @@
     <div class="mb-4 mx-auto">
       <span class="text-3xl">SSW LinkAuditor</span>
     </div>
+    <div class="mb-2 mx-auto">
+      <button
+        on:click|preventDefault={loginGmail}
+        type="button"
+        disabled={loading}
+        class="bg-red-700 hover:bg-blue-500 text-white font-semibold block
+        hover:text-grey-100 py-2 px-4 border border-blue-500
+        hover:border-transparent rounded">
+        Sign Up Using Google
+        {#if loading}...{/if}
+      </button>
+    </div>
+    <div class="mb-4 mx-auto">
+      <button
+        on:click|preventDefault={loginFacebook}
+        type="button"
+        disabled={loading}
+        class="bg-blue-700 hover:bg-blue-500 text-white font-semibold block
+        hover:text-grey-100 py-2 px-4 border border-blue-500
+        hover:border-transparent rounded">
+        Sign Up Using Facebook
+        {#if loading}...{/if}
+      </button>
+    </div>
+    <hr class="mb-4" />
     <div class="mb-4">
       <TextField
         bind:value={username}
