453f9f06f4df6e5bae779aef7d6e498f67fbd616
anthony <nvhoanganh1909@gmail.com>
2020-05-14 01:31:26 +1000

    crawler

--- link-checker-go/main.go
+++ link-checker-go/main.go
@@ -119,18 +119,70 @@ func crawl(link Link, ch chan Link, linkch chan LinkStatus) {
 }
 
 func parseUrl(startUrl string, url string) string {
-	if !strings.HasPrefix(url, "http") {
-		// strip out the filename (e.g. .aspx  or .asp or .php)
-		r := regexp.MustCompile(`(?P<file>[^\/]*.[a-z]{2,})$`)
-		if len(r.FindStringSubmatch(startUrl)) > 0 {
-			fileName := r.FindStringSubmatch(startUrl)[0]
-			startUrl = strings.ReplaceAll(startUrl, fileName, "")
-		}
-		u, _ := urlP.Parse(startUrl)
-		u.Path = path.Join(u.Path, url)
-		url = u.String()
+	url = strings.TrimSpace(url)
+
+	if strings.Index(url, "#") > 0 {
+		url = strings.Split(url, "#")[0]
 	}
-	return url
+
+	if strings.Index(url, "?") > 0 {
+		url = strings.Split(url, "?")[0]
+	}
+
+	if strings.HasPrefix(url, "http") {
+		// already a fully formed url
+		return url
+	}
+
+	sUrl, _ := urlP.Parse(startUrl)
+
+	if strings.HasPrefix(url, "//") {
+		// fmt.Println(url, "1.becomes", "https:"+url)
+		return sUrl.Scheme + "://" + url
+	} else if strings.HasPrefix(url, "/") {
+		// start with / => this is related to the full path
+		u, _ := urlP.Parse(startUrl)
+		// fmt.Println(url, "2.becomes", protocol+u.Hostname()+url)
+		return sUrl.Scheme + "://" + u.Hostname() + url
+	} else {
+
+		UrlPath := sUrl.Path
+		// strip out the filename (e.g. .aspx  or .asp or .php)
+		r := regexp.MustCompile(`(?P<file>\/[^\/]+\.[a-z0-9]+/*)$`)
+
+		// check if the path is ASPX or PHP
+		if len(r.FindStringSubmatch(UrlPath)) > 0 {
+			// fmt.Println(startUrl, "path is a file (e.g. php/aspx)")
+			fileName := r.FindStringSubmatch(UrlPath)[0]
+			UrlPath = strings.ReplaceAll(UrlPath, fileName, "")
+		}
+
+		baseUrl := sUrl.Scheme + "://" + sUrl.Hostname() + UrlPath
+		if !strings.HasSuffix(baseUrl, "/") {
+			baseUrl = baseUrl + "/"
+		}
+
+		u, _ := urlP.Parse(baseUrl)
+		u.Path = path.Join(u.Path, url)
+		// fmt.Println("base", startUrl, "dest", url, "new URL", u.String())
+		return u.String()
+	}
+}
+
+func isProtocolUrl(url string) bool {
+	r := regexp.MustCompile(`^[a-z]+:[^\/\/]`)
+	if len(r.FindStringSubmatch(url)) > 0 {
+		return true
+	}
+	return false
+}
+
+func isResourceFile(url string) bool {
+	r := regexp.MustCompile(`.*\.(mht|jpg|png|css|js|ico|gif|svg|mp3|ttf)`)
+	if len(r.FindStringSubmatch(url)) > 0 {
+		return true
+	}
+	return false
 }
 
 func main() {
@@ -142,22 +194,30 @@ func main() {
 	chAllUrls := make(chan LinkStatus)
 
 	crawling := 1
+
 	go crawl(startUrl, chUrls, chAllUrls)
 
 	for crawling >= 1 {
 		select {
 		case link := <-chUrls:
-			if strings.HasPrefix(link.url, "skype:") || strings.HasPrefix(link.url, "javascript:") || strings.HasPrefix(link.url, "#") || link.url == "" {
+
+			if isProtocolUrl(link.url) ||
+				strings.HasPrefix(link.url, "#") ||
+				strings.HasPrefix(link.url, "?") ||
+				link.url == "" {
 				continue
 			}
 
+			// fmt.Println("original url is", link.url, "on", link.srcUrl)
 			link.url = parseUrl(link.srcUrl, link.url)
 			_, crawled := allUrls[link.url]
+			// fmt.Println("parsed url is", link.url)
 
 			if !crawled {
 				allUrls[link.url] = LinkStatus{link.url, link.srcUrl, "", 0}
 				crawling++
-				if strings.Index(link.url, startUrl.url) >= 0 && link.linkType == "a" {
+				fmt.Println("Crawler:", crawling)
+				if strings.Index(link.url, startUrl.url) == 0 && link.linkType == "a" && !isResourceFile(link.url) {
 					go crawl(link, chUrls, chAllUrls)
 				} else {
 					go check(link, chAllUrls)
