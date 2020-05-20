9e6f7270d29d33df1d41ffdcbea39c695d34eee7
anthony <nvhoanganh1909@gmail.com>
2020-05-20 20:37:24 +1000

    style

--- ui/src/components/Tabs.svelte
+++ ui/src/components/Tabs.svelte
@@ -4,9 +4,8 @@
   export let build = {};
   export let displayMode = "";
 
-  let baseClass =
-    "bg-white inline-block py-2 px-4 textgrey hover:text-grey-800 font-semibold";
-  let active = " text-blue-700 border-l border-t border-r rounded-t";
+  let baseClass = "bg-white inline-block py-2 px-4 text-gray-600 font-semibold";
+  let active = " textgrey border-l border-t border-r rounded-t";
   $: totalHtmlIssues = (build.htmlErrors || 0) + (build.htmlWarnings || 0);
 
   let lhWarning = 0;
@@ -26,23 +25,21 @@
 
 <ul class="flex border-b">
   <li class="mr-1" class:-mb-px={displayMode === 'url'}>
-    <span class={baseClass + (displayMode === 'url' ? active : '')} href="#">
+    <span class={baseClass + (displayMode === 'url' ? active : '')}>
       <Navigate to={'/build/' + build.runId}>
         Links{build.uniqueBrokenLinks ? ` (${build.uniqueBrokenLinks})` : ''}
       </Navigate>
     </span>
   </li>
   <li class="mr-1" class:-mb-px={displayMode === 'html'}>
-    <span class={baseClass + (displayMode === 'html' ? active : '')} href="#">
+    <span class={baseClass + (displayMode === 'html' ? active : '')}>
       <Navigate to={'/htmlhint/' + build.runId}>
         HTML{totalHtmlIssues ? ` (${totalHtmlIssues})` : ''}
       </Navigate>
     </span>
   </li>
   <li class="mr-1" class:-mb-px={displayMode === 'lighthouse'}>
-    <span
-      class={baseClass + (displayMode === 'lighthouse' ? active : '')}
-      href="#">
+    <span class={baseClass + (displayMode === 'lighthouse' ? active : '')}>
       <Navigate to={'/lighthouse/' + build.runId}>
         Lighthouse Audit{lhWarning.length ? ` (${lhWarning.length})` : ''}
       </Navigate>
