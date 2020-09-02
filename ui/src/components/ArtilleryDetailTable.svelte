df774fd59af1f649730fbad3f97228a559fca5f9
unknown <67776356+tombui99@users.noreply.github.com>
2020-09-02 14:45:25 +1000

    Added components to be displayed

---
+++ ui/src/components/ArtilleryDetailTable.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { getArtilleryResult } from "../utils/utils";
+    export let value = {};
+
+    let details = value.summary;
+    $: atr = getArtilleryResult(details);
+  
+  </script>
+  
+  {#if atr.scenariosCompleted}
+    <span class="font-sans">Artillery</span>
+    <br>
+    <table>
+    <thead>
+        <tr>
+            <th class="px-4 py-2">Time </th>
+            <th class="px-4 py-2">scenariosCreated</th>
+            <th class="px-4 py-2">scenariosCompleted</th>
+            <th class="px-4 py-2">requestsCompleted</th>
+            <th class="px-4 py-2">latencyMedian</th>
+            <th class="px-4 py-2">rpsCount</th>
+        </tr>   
+    </thead> 
+    <tbody>
+        <tr>          
+            <td class="border px-4 py-2">{atr.timestamp}</td>
+            <td class="border px-4 py-2">{atr.scenariosCreated}</td>
+            <td class="border px-4 py-2">{atr.scenariosCompleted}</td>
+            <td class="border px-4 py-2">{atr.requestsCompleted}</td>
+            <td class="border px-4 py-2">{atr.latencyMedian}</td>
+            <td class="border px-4 py-2">{atr.rpsCount}</td>
+        </tr>  
+    </tbody>
+    </table>
+{/if}
+  
+
+  
+  
---
+++ ui/src/containers/ArtilleryReport.svelte
@@ -0,0 +1,63 @@
+<script>
+    import { onMount, onDestroy } from "svelte";
+    import { Navigate, navigateTo } from "svelte-router-spa";
+    import ArtilleryDetailTable from "../components/ArtilleryDetailTable.svelte";
+    import Breadcrumbs from "../components/Breadcrumbs.svelte";
+    import LoadingFlat from "../components/LoadingFlat.svelte";
+    import Icon from "../components/Icon.svelte";
+    import Tabs from "../components/Tabs.svelte";
+    import Toastr from "../components/Toastr.svelte";
+    import slug from "slug";
+    import {
+      getBuildDetails,
+      userApi,
+      userSession$,
+      getIgnoreList
+    } from "../stores";
+    import { printTimeDiff, CONSTS} from "../utils/utils";
+  
+    export let currentRoute;
+  
+    let loading;
+  
+    let promise = getBuildDetails(currentRoute.namedParams.run);
+    let runId;
+    let userNotLoginToast;
+  </script>
+  
+  <div class="container mx-auto">
+    <div class="bg-white shadow-lg rounded px-8 pt-6 mb-6 flex flex-col">
+  
+      {#if loading}
+        <LoadingFlat />
+      {:else}
+        {#await promise}
+          <LoadingFlat />
+        {:then data}
+          <Tabs build={data ? data.summary : {}} displayMode="artillery" />
+        
+          <Breadcrumbs
+            build={data ? data.summary : {}}
+            runId={currentRoute.namedParams.id}
+            displayMode="Artillery Load Test" />
+
+            <ArtilleryDetailTable value={data} />
+        
+        {:catch error}
+          <p class="text-red-600 mx-auto text-2xl py-8">{error.message}</p>
+        {/await}
+      {/if}
+    </div>
+  </div>
+  
+  <Toastr bind:show={userNotLoginToast} timeout={10000} mode="warn">
+    <p>Sign in to unlock this feature!</p>
+    <p class="text-sm pt-2">
+      <span
+        class="inline-block align-baseline font-bold text-sm text-blue
+        hover:text-blue-darker">
+        <Navigate to="/login">Sign in</Navigate>
+      </span>
+    </p>
+  </Toastr>
+  
