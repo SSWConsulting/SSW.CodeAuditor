f7f751a8f13bdf392f5e5c3a64508b2ade8e62a8
unknown <67776356+tombui99@users.noreply.github.com>
2020-09-16 17:40:10 +1000

    Fixed HTML Hint broken url

--- ui/src/containers/HtmlHints.svelte
+++ ui/src/containers/HtmlHints.svelte
@@ -30,7 +30,7 @@
   let promise = getHtmlHints(runId);
   async function getHtmlHints(id) {
     const d = await fetch(
-      `${CONSTS.URLChecker}/htmlhint/${id}.json`
+      `${CONSTS.BlobURL}/htmlhint/${id}.json`
     );
     let htmlHint = await d.json();
 
