40716274547899ead3505faece1e35f83abf4ec1
tombui99 <tom.quang.bui@gmail.com>
2021-03-31 11:59:46 +1100

    Header and Footer UI Fix

--- ui/src/containers/Public.svelte
+++ ui/src/containers/Public.svelte
@@ -331,18 +331,6 @@
         width="200"
         height="300" />
     </a>
-    <p
-      class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2
-      sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-      © 2020 SSW —
-      <a
-        href="https://twitter.com/ssw_tv"
-        class="text-gray-600 ml-1"
-        rel="noopener noreferrer"
-        target="_blank">
-        @ssw_tv
-      </a>
-    </p>
     <span
       class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
       <a
