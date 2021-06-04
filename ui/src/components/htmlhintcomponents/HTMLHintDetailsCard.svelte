93b9c98663038a8c2bf69cf2bd5dccee8d63f2df
tombui99 <tom.quang.bui@gmail.com>
2021-06-04 12:34:13 +1000

    Added fontawesome

--- ui/src/components/htmlhintcomponents/HTMLHintDetailsCard.svelte
+++ ui/src/components/htmlhintcomponents/HTMLHintDetailsCard.svelte
@@ -74,14 +74,7 @@
           {#if htmlRules}
             <p class="inline">HTML Rules Scanned: {htmlRules.selectedRules.split(/[,]+/).length}</p>
             <span type="button" class="inline cursor-pointer" on:click={handleClick}>
-              <svg
-              class="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"><path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd" /></svg>
+              <i class="fas fa-angle-down"></i>
             </span>
               {#if isCollapsedRules}
                 {#each htmlRules.selectedRules.split(/[,]+/) as rule}
