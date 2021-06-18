1a488b420c9ea5da34712195387faee2605873d8
tombui99 <tom.quang.bui@gmail.com>
2021-06-18 10:30:09 +1000

    added instruction to add custom rules

--- ui/src/containers/HowItWorks.svelte
+++ ui/src/containers/HowItWorks.svelte
@@ -67,6 +67,37 @@
     (make sure to include the full URL with 'https')
   5. Once scan is complete, a result script will display which gives you a link to your scan result page
   \`\`\``;
+
+  const addingCustomRule = `
+  ## How to Add Custom HTML Hint Rules
+  #### 1. Go to our GitHub and clone the project at https://github.com/SSWConsulting/SSW.CodeAuditor
+  #### 2. Have a look at [HtmlHint Rules](https://github.com/htmlhint/HTMLHint/tree/master/src/core/rules) to view sample existing rules
+  #### 3. In your local repo, go to \`\`\` docker/customHtmlRules.js \`\`\`
+  #### 4. Add your custom Rule under \`\`\`// Add new custom rule below\`\`\` using the following template:  
+  \`\`\` js
+  HTMLHint.addRule({
+        id: "your-custom-rule-id",
+        description: "Your custom rule description",
+        init: function (parser, reporter) {
+          // Your rule logic
+        }
+      })
+  \`\`\`
+  #### 5. Go to \`\`\`docker/api.js\`\`\`: On the last export named \`\`\`htmlHintConfig\`\`\`, add your new custom rule id to the list using the following format:
+  \`\`\` js
+  exports.htmlHintConfig = {
+    your-custom-rule-id: true,
+    ...
+    }
+  \`\`\`
+  #### 6. Go to \`\`\`ui/src/utils/utils.js\`\`\` On the last export named \`\`\`customHtmlHintRules\`\`\` add your new custom rule id to the list using the following format:
+  \`\`\` js
+  export const customHtmlHintRules = [
+   { rule: "your-custom-rule-id" },	
+   ...
+  ];
+  \`\`\`
+  #### 7. Make a Pull Request and have it checked by CodeAuditor Team`;
 </script>
 
 <div class="container mx-auto">
@@ -110,6 +141,9 @@
       </article>
       <article class="markdown-body mt-5">
         {@html marked(instructionSteps)}
+      </article>
+      <article class="markdown-body mt-5">
+        {@html marked(addingCustomRule)}
       </article>
       <article class="markdown-body mt-5">
         <h3>Video</h3>
