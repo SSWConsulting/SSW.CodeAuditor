aeb88aa25c3554aeae89410eaeaa9aff6adbec70
tombui99 <tom.quang.bui@gmail.com>
2021-07-06 10:23:46 +1000

    Added Mocha unit testing

---
+++ docker/test/meta-tags-must-not-redirect.spec.js
@@ -0,0 +1,25 @@
+const expect = require('expect.js')
+const HTMLHint = require("htmlhint").default;
+
+const ruldId = 'meta-tag-must-not-redirect'
+
+const ruleOptions = {}
+
+const {addCustomHtmlRule} = require('../customHtmlRules')
+
+ruleOptions[ruldId] = true
+
+describe(`Rules: ${ruldId}`, () => {
+  addCustomHtmlRule();
+  it('Meta tags that does not refresh should not result in an error', () => {
+    const code = '<meta name="description" />'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(0)
+  })
+
+  it('Meta tags that refresh should result in an error', () => {
+    const code = '<meta http-equiv="refresh" content="5" />'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(1)
+  })
+})
---
+++ docker/test/page-must-not-show-email-addresses.spec.js
@@ -0,0 +1,25 @@
+const expect = require('expect.js')
+const HTMLHint = require("htmlhint").default;
+
+const ruldId = 'page-must-not-show-email-addresses'
+
+const ruleOptions = {}
+
+const {addCustomHtmlRule} = require('../customHtmlRules')
+
+ruleOptions[ruldId] = true
+
+describe(`Rules: ${ruldId}`, () => {
+  addCustomHtmlRule();
+  it('Page that does not show email addresses should not result in an error', () => {
+    const code = '<a href="...">email address</a>'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(0)
+  })
+
+  it('Page that shows email address should result in an error', () => {
+    const code = '<a href="...">tomisawesome@gmail.com</a>'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(1)
+  })
+})
