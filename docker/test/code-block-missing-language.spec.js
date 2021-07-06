19816367b68f4b1295f0df022080a1553abc5548
tombui99 <tom.quang.bui@gmail.com>
2021-07-06 11:15:43 +1000

    Added unit test for previous custom rules

---
+++ docker/test/code-block-missing-language.spec.js
@@ -0,0 +1,37 @@
+const expect = require('expect.js')
+const HTMLHint = require("htmlhint").default;
+
+const ruldId = 'code-block-missing-language'
+
+const ruleOptions = {}
+
+const {addCustomHtmlRule} = require('../customHtmlRules')
+
+ruleOptions[ruldId] = true
+
+describe(`Rules: ${ruldId}`, () => {
+  addCustomHtmlRule();
+  it('Code block with data language specifier should not result in an error', () => {
+    const code = '<pre data-language="javascript">Some Code</pre>'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(0)
+  })
+
+  it('Code block with class language specifier should not result in an error', () => {
+    const code = '<pre class="language-xml">Some Code</pre>'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(0)
+  })
+
+  it('Code block without class language specifier should not result in an error', () => {
+    const code = '<pre class="">Some Code</pre>'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(1)
+  })
+
+  it('Code block without data language specifier should result in an error', () => {
+    const code = '<pre data-language="">Some Code</pre>'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(1)
+  })
+})
---
+++ docker/test/grammar-scrum-terms.spec.js
@@ -0,0 +1,25 @@
+const expect = require('expect.js')
+const HTMLHint = require("htmlhint").default;
+
+const ruldId = 'grammar-scrum-terms'
+
+const ruleOptions = {}
+
+const {addCustomHtmlRule} = require('../customHtmlRules')
+
+ruleOptions[ruldId] = true
+
+describe(`Rules: ${ruldId}`, () => {
+  addCustomHtmlRule();
+  it('Scrum terms that are cased correctly should not result in an error', () => {
+    const code = '<p>Scrum, Sprint, Product Owner, Scrum Master, Product Backlog, Sprint Review, Sprint Planning, Sprint Retrospective</p>'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(0)
+  })
+
+  it('Scrum terms that are cased correctly should result in an error', () => {
+    const code = '<p>scrum, sprint, product owner, scrum master, product backlog, sprint review, sprint planning, sprint retrospective</p>'
+    const messages = HTMLHint.verify(code, ruleOptions)
+    expect(messages.length).to.be(8)
+  })
+})
