15713cce2237baa89b500ee63f2d916b81e848d8
tombui99 <tom.quang.bui@gmail.com>
2023-06-08 09:38:13 +1000

    Improved UI per designer feedback

--- ui/src/components/scancomparecomponents/ScanCompareListItem.svelte
+++ ui/src/components/scancomparecomponents/ScanCompareListItem.svelte
@@ -10,53 +10,47 @@
 <h2 class="font-bold font-sans text-gray-600 mb-2">LINKS</h2>
 <div class="grid grid-cols-1 gap-x-5 text-lg">
   <div class="text-start">
-    <span class="block whitespace-no-wrap font-sans"
-      >404 Errors
-      <i class="fas fa-exclamation-circle" style="color: red" />
-    </span>
     <span
-      class="font-sans font-bold block lg:inline-block"
+      class="font-sans font-bold block lg:inline-block mt-3"
       class:text-red-600={value.totalUnique404 > 0}
       class:text-gray-600={value.totalUnique404 === 0}
     >
-      {value.totalUnique404}
-      {Object.keys(comparisonDifferences).length > 0
-        ? `(
-        ${comparisonDifferences.brokenLinksDifference > 0 ? "⬆️" : "⬇️"} 
-        ${Math.abs(comparisonDifferences.brokenLinksDifference)} )`
-        : ""}
+      <span class="text-4xl">{value.totalUnique404}</span>
+      {#if Object.keys(comparisonDifferences).length > 0}
+        <i class="fas {comparisonDifferences.brokenLinksDifference > 0 ? "fa-arrow-up" : "fa-arrow-down"}" style="color: #4B5563" />
+        <span class="text-gray-600">{Math.abs(comparisonDifferences.brokenLinksDifference)}</span>
+      {/if}
+    </span>
+    <span class="block whitespace-no-wrap font-sans mt-2">404 Errors
+      <i class="fas fa-exclamation-circle" style="color: red" />
     </span>
   </div>
 </div>
 
-<h2 class="font-bold font-sans text-gray-600 mt-3 mb-2">CODE</h2>
+<h2 class="font-bold font-sans text-gray-600 mt-8 mb-2">CODE</h2>
 <div class="grid grid-cols-1 gap-x-5 text-lg">
   <div class="text-start">
-    <span class="block font-sans"
-      >HTML Warnings
+    <span class="font-sans font-bold block lg:inline-block text-yellow-600 mt-3">
+      <span class="text-4xl">{numberWithCommas(value.htmlWarnings)}</span>
+      {#if Object.keys(comparisonDifferences).length > 0}
+        <i class="fas {comparisonDifferences.codeWarningDifference > 0 ? "fa-arrow-up" : "fa-arrow-down"}" style="color: #4B5563" />
+        <span class="text-gray-600">{Math.abs(comparisonDifferences.codeWarningDifference)}</span>
+      {/if}
+    </span>
+    <span class="block whitespace-no-wrap font-sans mt-2">HTML Warnings
       <i class="fas fa-exclamation-triangle" style="color: #d69e2e" />
     </span>
-    <span class="font-sans font-bold block lg:inline-block text-yellow-600"
-      >{numberWithCommas(value.htmlWarnings)}
-      {Object.keys(comparisonDifferences).length > 0
-        ? `(
-            ${comparisonDifferences.codeWarningDifference > 0 ? "⬆️" : "⬇️"} 
-            ${Math.abs(comparisonDifferences.codeWarningDifference)} )`
-        : ""}</span
-    >
   </div>
-  <div class="text-start mt-2">
-    <span class="block whitespace-no-wrap font-sans"
-      >HTML Errors
-      <i class="fas fa-exclamation-circle" style="color: red" />
-    </span>
+  <div class="text-start mt-6">
     <span class="font-sans font-bold block lg:inline-block text-red-600">
-      {numberWithCommas(value.htmlErrors)}
-      {Object.keys(comparisonDifferences).length > 0
-        ? `(
-        ${comparisonDifferences.codeErrorDifference > 0 ? "⬆️" : "⬇️"} 
-        ${Math.abs(comparisonDifferences.codeErrorDifference)} )`
-        : ""}
+      <span class="text-4xl">{numberWithCommas(value.htmlErrors)}</span>
+      {#if Object.keys(comparisonDifferences).length > 0}
+        <i class="fas {comparisonDifferences.codeErrorDifference > 0 ? "fa-arrow-up" : "fa-arrow-down"}" style="color: #4B5563" />
+        <span class="text-gray-600">{Math.abs(comparisonDifferences.codeErrorDifference)}</span>
+      {/if}
+    </span>
+    <span class="block whitespace-no-wrap font-sans mt-2">HTML Errors
+      <i class="fas fa-exclamation-circle" style="color: red" />
     </span>
   </div>
 </div>
--- ui/src/containers/ScanCompare.svelte
+++ ui/src/containers/ScanCompare.svelte
@@ -40,27 +40,24 @@
     >
   </div>
   <div class="grid grid-cols-2 gap-x-6 text-center mt-8">
-    <div class="text-lg mb-5">Select Past Scan to compare:</div>
-    <div class="text-lg mb-5">Latest Scan at</div>
+    <div class="text-lg text-gray-600 mb-2">Select Past Scan to compare:</div>
+    <div class="text-lg text-gray-600 mb-2">Latest Scan at</div>
     <div>
       <select
         bind:value={selectedScan}
         on:change={() => getDifferences()}
-        class="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500"
+        class="mb-4 text-gray-900 text-lg font-sans font-bold"
       >
         {#each allScans.slice(1) as scan}
           <option value={scan}>
-            {format(new Date(scan.buildDate), "dd LLL y")} at {format(
-              new Date(scan.buildDate),
-              "h:mm bbb"
-            )}
+            {format(new Date(scan.buildDate), "h:mm bbb, dd LLL y")}
           </option>
         {/each}
       </select>
     </div>
-    <div class="font-sans font-bold text-xl text-gray-900">
+    <div class="font-sans font-bold text-lg text-gray-900">
       {allScans.length > 0
-        ? format(new Date(allScans[0].buildDate), "dd LLL y, h:mm bbb")
+        ? format(new Date(allScans[0].buildDate), "h:mm bbb, dd LLL y")
         : ""}
     </div>
     <div>
