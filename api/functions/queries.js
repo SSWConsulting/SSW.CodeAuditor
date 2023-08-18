6750efd604fd8933a3d47c9afd0fc61326b45d5d
tombui99 <tom.quang.bui@gmail.com>
2023-08-18 18:30:44 +1000

    TECH DEBT - Improve Azure Queries performance

--- api/functions/queries.js
+++ api/functions/queries.js
@@ -12,6 +12,11 @@ const {
 const azure = require('azure-storage');
 const { TableClient, AzureNamedKeyCredential, odata } = require('@azure/data-tables');
 
+const account = process.env.AZURE_STORAGE_ACCOUNT;
+const accountKey = process.env.AZURE_STORAGE_ACCESS_KEY;
+const credential = new AzureNamedKeyCredential(account, accountKey);
+const azureUrl = `https://${account}.table.core.windows.net`;
+
 exports.getConfig = (api) =>
 	new Promise((resolve, reject) => {
 		getService().retrieveEntity(
@@ -25,17 +30,11 @@ exports.getConfig = (api) =>
 		);
 	});
 
-// TODO: Tech debt - Replace all exisiting deprecated azure-storage library with new one using similar template as below
-const account = process.env.AZURE_STORAGE_ACCOUNT;
-const accountKey = process.env.AZURE_STORAGE_ACCESS_KEY;
-const credential = new AzureNamedKeyCredential(account, accountKey);
-const azureUrl = `https://${account}.table.core.windows.net`;
-
 exports.getScanDetails = (runId) => 
 	getRun(runId).then((doc) =>
 		new Promise(async (resolve) => {
 			const entity = new TableClient(azureUrl, TABLE.ScanResults, credential).listEntities({
-				queryOptions: { filter: odata`runId eq ${doc.runId}` }
+				queryOptions: { filter: odata`PartitionKey eq ${doc.apikey} and runId eq ${doc.runId}` }
 			});
 			let result = []
 			for await (const item of entity) {
@@ -45,64 +44,67 @@ exports.getScanDetails = (runId) =>
 		}));
 
 exports.getIgnoredUrls = (api) =>
-	getTableRows(
-		TABLE.IgnoredUrls,
-		new azure.TableQuery().where('PartitionKey eq ?', api)
-	);
+	new Promise(async (resolve) => {
+		const entity = new TableClient(azureUrl, TABLE.IgnoredUrls, credential).listEntities({
+			queryOptions: { filter: odata`PartitionKey eq ${api}` }
+		});
+		let result = []
+		for await (const item of entity) {
+			result.push(item);
+		}
+		resolve(result)
+	})
 
 exports.getPerformanceThreshold = async (api, url) => {
-	const val = await getTableRows(
-		TABLE.PerformanceThreshold,
-		new azure.TableQuery()
-		.where('PartitionKey eq ?', api)
-		.and('RowKey eq ?', slug(url))
-	);
-
-	if (val && val.length > 0) {
-		return val[0];
-	}
-	return null;
+	new Promise(async (resolve) => {
+		const entity = new TableClient(azureUrl, TABLE.PerformanceThreshold, credential).listEntities({
+			queryOptions: { filter: odata`PartitionKey eq ${api} and RowKey eq ${slug(url)}` }
+		});
+		let result = []
+		for await (const item of entity) {
+			result.push(item);
+		}
+		resolve(result)
+	})
 };
 
 exports.getLoadThreshold = async (api, url) => {
-	const val = await getTableRows(
-		TABLE.LoadThreshold,
-		new azure.TableQuery()
-		.where('PartitionKey eq ?', api)
-		.and('RowKey eq ?', slug(url))
-	);
-
-	if (val && val.length > 0) {
-		return val[0];
-	}
-	return null;
+	new Promise(async (resolve) => {
+		const entity = new TableClient(azureUrl, TABLE.LoadThreshold, credential).listEntities({
+			queryOptions: { filter: odata`PartitionKey eq ${api} and RowKey eq ${slug(url)}` }
+		});
+		let result = []
+		for await (const item of entity) {
+			result.push(item);
+		}
+		resolve(result)
+	})
 };
 
 exports.getHTMLHintRules = async (api, url) => {
-	const val = await getTableRows(
-		TABLE.htmlhintrules,
-		new azure.TableQuery()
-		.where('PartitionKey eq ?', api)
-		.and('RowKey eq ?', slug(url))
-	);
-
-	if (val && val.length > 0) {
-		return val[0];
-	}
-	return null;
+	new Promise(async (resolve) => {
+		const entity = new TableClient(azureUrl, TABLE.htmlhintrules, credential).listEntities({
+			queryOptions: { filter: odata`PartitionKey eq ${api} and RowKey eq ${slug(url)}` }
+		});
+		let result = []
+		for await (const item of entity) {
+			result.push(item);
+		}
+		resolve(result)
+	})
 };
 
 exports.getHTMLHintRulesByRunId = async (runId) => {
-	const val = await getTableRows(
-		TABLE.htmlhintrules,
-		new azure.TableQuery()
-		.where('RowKey eq ?', runId)
-	);
-
-	if (val && val.length > 0) {
-		return val[0];
-	}
-	return null;
+	new Promise(async (resolve) => {
+		const entity = new TableClient(azureUrl, TABLE.htmlhintrules, credential).listEntities({
+			queryOptions: { filter: odata`RowKey eq ${runId}` }
+		});
+		let result = []
+		for await (const item of entity) {
+			result.push(item);
+		}
+		resolve(result)
+	})
 };
 
 exports.getPersonalSummary = (api, showAll) =>
