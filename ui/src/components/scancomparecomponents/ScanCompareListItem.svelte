cd9f572779341f27380c83af34521b0c45cfbca6
Zachary Keeping <zacharykeeping@icloud.com>
2023-09-07 09:07:38 +1000

    Update casing

--- ui/src/components/scancomparecomponents/ScanCompareListItem.svelte
+++ ui/src/components/scancomparecomponents/ScanCompareListItem.svelte
@@ -24,7 +24,7 @@
     <span class="textgrey">{numberWithCommas(Math.abs(comparisonDifferences.brokenLinksDifference))}</span>
     {/if}
   </div>
-  <h3 class="col-span-2 font-bold font-sans textgrey">Broken Links {value.totalUnique404Existing !== undefined ? `(${value.totalUnique404Existing || 0} existing)` : ''}</h3>
+  <h3 class="col-span-2 font-bold font-sans textgrey">Broken Links {value.totalUnique404Existing !== undefined ? `(${value.totalUnique404Existing || 0} Existing)` : ''}</h3>
 </div>
 
 <div class="grid grid-cols-2 grid-rows-2 text-start font-sans font-bold mt-8 mb-2">
