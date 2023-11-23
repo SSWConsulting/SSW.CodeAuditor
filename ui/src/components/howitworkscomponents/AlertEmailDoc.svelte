742fe9b25e12ae04f9cd1342392d3e7061ea044e
tombui99 <tom.quang.bui@gmail.com>
2023-11-24 10:15:26 +1100

    Wrap all markdown images in img tags

--- ui/src/components/howitworkscomponents/AlertEmailDoc.svelte
+++ ui/src/components/howitworkscomponents/AlertEmailDoc.svelte
@@ -4,15 +4,42 @@
     const emailAlertInstruction = 
 `# How to send automated Email Alert for future scans
 #### 1. Click on "Send Email Alerts" to open the modal
-![Image](https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/8fd73e0a-9b23-4bc6-b7f6-fdf35a04a46a)
+
+<a href="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/8fd73e0a-9b23-4bc6-b7f6-fdf35a04a46a" target="_blank">
+    <img
+        class="object-cover object-center rounded bordered"
+        style="max-width: 85%;
+        alt="hero"
+        src="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/8fd73e0a-9b23-4bc6-b7f6-fdf35a04a46a"
+    />
+</a>
+
 **Figure: Send Email Alerts button**
 
 #### 2. Add or remove email addresses to receive alert
-![Image](https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/b51a2548-d452-4496-89ab-41fde0a2500a)
+
+<a href="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/b51a2548-d452-4496-89ab-41fde0a2500a" target="_blank">
+    <img
+        class="object-cover object-center rounded bordered"
+        style="max-width: 85%;
+        alt="hero"
+        src="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/b51a2548-d452-4496-89ab-41fde0a2500a"
+    />
+</a>
+
 **Figure: Email alerts modal**
 
 #### 3. After your next scan has completed, the email addresses will receive automated email alerts 
-![Image](https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/69b44d1b-22b3-477c-8ab4-19560d88e64d)
+
+<a href="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/69b44d1b-22b3-477c-8ab4-19560d88e64d" target="_blank">
+    <img
+        class="object-cover object-center rounded bordered"
+        style="max-width: 85%;
+        alt="hero"
+        src="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/69b44d1b-22b3-477c-8ab4-19560d88e64d"
+    />
+</a>
+
 **Figure: Sample email alerts**`
 </script>
 
--- ui/src/components/howitworkscomponents/ScanCompareDoc.svelte
+++ ui/src/components/howitworkscomponents/ScanCompareDoc.svelte
@@ -4,11 +4,29 @@
     const scanCompareInstruction = `
   # How to compare between scans
   #### 1. Click on "Compare to latest scan" to go to scan compare page
-  ![Image](https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/7c995453-5b40-4ef2-94fb-0ea1feeb7182)
+
+  <a href="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/7c995453-5b40-4ef2-94fb-0ea1feeb7182" target="_blank">
+    <img
+        class="object-cover object-center rounded bordered"
+        style="max-width: 85%;
+        alt="hero"
+        src="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/7c995453-5b40-4ef2-94fb-0ea1feeb7182"
+    />
+  </a>
+
   **Figure: Scan compare button**
 
   #### 2. Select in the dropdown list to choose which previous scan you want to compare to the latest one
-  ![Image](https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/2e2f25a1-1c5f-4f82-afe2-72488b530666)
+  
+  <a href="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/2e2f25a1-1c5f-4f82-afe2-72488b530666" target="_blank">
+    <img
+        class="object-cover object-center rounded bordered"
+        style="max-width: 85%;
+        alt="hero"
+        src="https://github.com/SSWConsulting/SSW.CodeAuditor/assets/67776356/2e2f25a1-1c5f-4f82-afe2-72488b530666"
+    />
+  </a>
+
   **Figure: Scan comparison page**
   `
 </script>
