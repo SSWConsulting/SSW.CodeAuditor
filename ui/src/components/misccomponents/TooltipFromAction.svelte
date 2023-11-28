5c6ec6c069bd7afb5c09d77b9df0d4fc2f5f365b
Zachary Keeping <zacharykeeping@icloud.com>
2023-11-28 11:38:09 +1100

    Add tooltip functionality to ignore buttons in HtmlErrorsByReason and HtmlErrorsBySource components

--- ui/src/components/misccomponents/TooltipFromAction.svelte
+++ ui/src/components/misccomponents/TooltipFromAction.svelte
@@ -1,19 +1,20 @@
 <script>
-	export let title;
-	export let x;
-	export let y;
+  export let title;
+  export let x;
+  export let y;
 </script>
-<div style="
-		top: {y + 5}px;
-		left: {x + 5}px;">{title}</div>
+
+<div style="top: {y + 5}px; left: {x + 5}px;">
+  <i class="fa-solid fa-circle-info text-blue-600"></i> {title}
+</div>
 
 <style>
-	div {
-		border: 1px solid #ddd;
-		box-shadow: 1px 1px 1px #ddd;
-		background: white;
-		border-radius: 4px;
-		padding: 4px;
-		position: absolute;
-	}
+  div {
+    border: 1px solid #ddd;
+    box-shadow: 1px 1px 1px #ddd;
+    background: white;
+    border-radius: 4px;
+    padding: 4px;
+    position: absolute;
+  }
 </style>
