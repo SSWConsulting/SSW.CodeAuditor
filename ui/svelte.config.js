b999acfa18d6335436ee51b77da7188a63aaa75b
tombui99 <tom.quang.bui@gmai.com>
2024-03-14 12:15:57 +0700

    Render Svelte content statically

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,6 +1,18 @@
 // svelte.config.js
-const sveltePreprocess = require('svelte-preprocess');
+import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-module.exports = {
+export default {
 	preprocess: [sveltePreprocess()],
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
+	}
 };
